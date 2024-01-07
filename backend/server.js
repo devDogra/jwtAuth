b45caf47e7e04131d885c0c5856b01fd271e2763
Dev Aryan Dogra <devdogra1@gmail.com>
2024-01-07 23:41:42 +0530

    Final

---
+++ backend/server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/User.js');  
+const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken');
+
+const secret = 'this is my secret';
+
+const app = express(); 
+
+mongoose.connect('mongodb://127.0.0.1:27017/AuthG18')
+app.use(express.json());
+app.use(express.urlencoded()); 
+
+
+app.post('/register', async (req, res) => {
+    const user = req.body; 
+    if (!user.password || !user.username) {
+        res.send("Username and password are required"); 
+        return;     
+    }
+    if (user.password.length < 4) {
+        res.send("Password length must be >= 4");
+        return; 
+    }
+
+    const newUser = new User(user);
+    const saltRounds = 10;
+
+    const hashedPwd = await bcrypt.hash(newUser.password, saltRounds);
+    newUser.password = hashedPwd; 
+
+    try {
+        await newUser.save();
+        res.send("Registration successful"); 
+    } catch(err) {
+        res.send("Couldn't register account");
+    }
+
+})
+
+
+app.get('/profile', (req, res) => {
+    const token = req.headers.authorization;
+    try {
+        const payload = jwt.verify(token, secret);
+        res.send(`${payload.username}'s profile`);
+    } catch(err) {
+        console.log(err);
+        // Invalid token or no token
+        res.send("Invalid token");
+    }
+
+})
+
+
+app.post('/login', async (req, res) => {
+    const loginData = req.body; 
+    const account = await User.findOne().where('username').equals(loginData.username)
+
+    if (!account) {
+        res.send("No such account"); 
+        return;
+    }
+    // Account found
+    const match = await bcrypt.compare(loginData.password, account.password)
+    if (!match) {
+        res.send("Incorrect password"); 
+        return; 
+    }
+
+    const token = jwt.sign({ username: account.username }, secret);
+
+    res.send({
+        msg: "Login succesful",
+        token
+    })
+
+})
+
+
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000"); 
+})
+
+
